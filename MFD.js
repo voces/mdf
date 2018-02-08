c650cbf63a2fa5d8a1beca88dcee20ffdba75c1a
Robert Coe <chakrawc@gmail.com>
2018-02-08 11:52:53 -0800

    mdf refresh
    Replaced _generate with refresh. refresh will only run once at a time (returns same promise). Defining pool moved to separate _init, which is similarly protected.
    Moved dev script out of test and added to .gitignore.

--- MFD.js
+++ MFD.js
@@ -11,68 +11,93 @@ export default class MFD {
 		this.config = Object.assign( { multipleStatements: true }, config );
 		this.collections = {};
 
-		this._generate();
+		this._init();
 
 	}
 
-	async _generate() {
+	async _init() {
 
-		const pool = await mysql.createConnection( this.config );
-		Object.defineProperty( this, "pool", { value: pool } );
+		// Ensure only one init runs at a time
+		return this._init.promise || ( this._init.promise = ( async () => {
 
-		const [[ tables ], [ constraints ]] = await Promise.all( [
-			this.pool.execute( `SELECT table_name AS \`table\` FROM information_schema.tables${this.config.database ? " WHERE table_schema = ?" : ""};`, this.config.database ? [ this.config.database ] : [] ),
-			this.pool.execute( `SELECT constraint_name name, table_name table1, column_name column1, referenced_table_name table2, referenced_column_name column2 FROM information_schema.key_column_usage WHERE ( referenced_table_schema IS NOT NULL OR CONSTRAINT_NAME = 'PRIMARY' )${this.config.database ? " AND constraint_schema = ?" : ""};`, this.config.database ? [ this.config.database ] : [] )
-		] );
+			// Ensure we don't redefine the pool
+			if ( this.pool ) return;
 
-		const relations = {};
-		const keys = {};
+			Object.defineProperty( this, "pool", { value: await mysql.createConnection( this.config ) } );
 
-		const mdf = this;
-		tables.forEach( ( { table } ) => {
+			this.refresh();
 
-			this.collections[ table ] = class extends Model {
+			delete this._init.promise;
 
-				static get name() {
+		} )() );
 
-					return table;
+	}
 
-				}
+	async refresh() {
 
-				static get relations() {
+		return this.refresh.promise || ( this.refresh.promise = ( async () => {
 
-					return relations[ table ] || ( relations[ table ] = Object.assign(
-						constraints
-							.filter( relation => relation.name !== "PRIMARY" && relation.table1 === table )
-							.reduce( ( relations, relation ) => Object.assign( relations, { [ relation.column1 ]: {
-								source: relation.column1,
-								target: {
-									table: mdf.collections[ relation.table2 ],
-									column: relation.column2
-								},
-								singleton: true
-							} } ), {} ),
-						constraints
-							.filter( relation => relation.name !== "PRIMARY" && ( relation.table1 === table || relation.table2 === table ) )
-							.reduce( ( constraints, constraint ) => Object.assign( constraints, { [ constraint.name ]: {
-								source: constraint.table1 === table ? constraint.column1 : constraint.column2,
-								target: {
-									table: mdf.collections[ constraint.table1 === table ? constraint.table2 : constraint.table1 ],
-									column: constraint.table1 === table ? constraint.column2 : constraint.column1
-								}
-							} } ), {} ) ) );
+			await this._init();
 
-				}
+			const [[ tables ], [ constraints ]] = await Promise.all( [
+				this.pool.execute( `SELECT table_name AS \`table\` FROM information_schema.tables${this.config.database ? " WHERE table_schema = ?" : ""};`, this.config.database ? [ this.config.database ] : [] ),
+				this.pool.execute( `SELECT constraint_name name, table_name table1, column_name column1, referenced_table_name table2, referenced_column_name column2 FROM information_schema.key_column_usage WHERE ( referenced_table_schema IS NOT NULL OR CONSTRAINT_NAME = 'PRIMARY' )${this.config.database ? " AND constraint_schema = ?" : ""};`, this.config.database ? [ this.config.database ] : [] )
+			] );
 
-				static get key() {
+			const relations = {};
+			const keys = {};
 
-					return keys[ table ] || ( keys[ table ] = constraints.filter( relation => relation.table1 === table && relation.name === "PRIMARY" ).map( relation => relation.column1 ) );
+			const mdf = this;
+			tables.forEach( ( { table } ) => {
 
-				}
+				this.collections[ table ] = class extends Model {
 
-			};
+					static get name() {
 
-		} );
+						return table;
+
+					}
+
+					static get relations() {
+
+						return relations[ table ] || ( relations[ table ] = Object.assign(
+							constraints
+								.filter( relation => relation.name !== "PRIMARY" && relation.table1 === table )
+								.reduce( ( relations, relation ) => Object.assign( relations, { [ relation.column1 ]: {
+									source: relation.column1,
+									target: {
+										table: mdf.collections[ relation.table2 ],
+										column: relation.column2
+									},
+									singleton: true
+								} } ), {} ),
+							constraints
+								.filter( relation => relation.name !== "PRIMARY" && ( relation.table1 === table || relation.table2 === table ) )
+								.reduce( ( constraints, constraint ) => Object.assign( constraints, { [ constraint.name ]: {
+									source: constraint.table1 === table ? constraint.column1 : constraint.column2,
+									target: {
+										table: mdf.collections[ constraint.table1 === table ? constraint.table2 : constraint.table1 ],
+										column: constraint.table1 === table ? constraint.column2 : constraint.column1
+									}
+								} } ), {} ) ) );
+
+					}
+
+					static get key() {
+
+						return keys[ table ] || ( keys[ table ] = constraints.filter( relation => relation.table1 === table && relation.name === "PRIMARY" ).map( relation => relation.column1 ) );
+
+					}
+
+				};
+
+			} );
+
+			delete this.refresh.promise;
+
+			return this.collections;
+
+		} )() );
 
 	}
 
